1b6e63b6b810aa70b3cfb718084bb568274b617f
jhunethan <jhunethan23@gmail.com>
2021-07-12 12:07:54 +0100

    grid structure added

---
+++ src/components/Display/Display.js
@@ -0,0 +1,26 @@
+import "./Display.css";
+
+export default function Display() {
+  return (
+    <main>
+      <h1>Results</h1>
+      <section className="display__grid">
+        <div className="display__card"></div>
+        <div className="display__card"></div>
+        <div className="display__card"></div>
+        <div className="display__card"></div>
+        <div className="display__card"></div>
+        <div className="display__card"></div>
+        <div className="display__card"></div>
+        <div className="display__card"></div>
+        <div className="display__card"></div>
+        <div className="display__card"></div>
+        <div className="display__card"></div>
+        <div className="display__card"></div>
+        <div className="display__card"></div>
+        <div className="display__card"></div>
+        <div className="display__card"></div>
+      </section>
+    </main>
+  );
+}
