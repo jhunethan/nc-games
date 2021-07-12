7110e622913fc291582c752860c03ce9573afd8a
jhunethan <jhunethan23@gmail.com>
2021-07-12 12:58:45 +0100

    shows a list of all reviews

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,25 +1,41 @@
+import { useState, useEffect } from "react";
+import { getReviews } from "../../utils/api";
 import "./Display.css";
+import dateFormat from "dateformat";
 
 export default function Display() {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    getReviews().then((response) => {
+      const { reviews } = response.data;
+      console.log(reviews[0]);
+      setReviews(reviews);
+    });
+  }, []);
+
   return (
     <main>
       <h1>Reviews</h1>
       <section className="display__grid">
-        <div className="display__card"></div>
-        <div className="display__card"></div>
-        <div className="display__card"></div>
-        <div className="display__card"></div>
-        <div className="display__card"></div>
-        <div className="display__card"></div>
-        <div className="display__card"></div>
-        <div className="display__card"></div>
-        <div className="display__card"></div>
-        <div className="display__card"></div>
-        <div className="display__card"></div>
-        <div className="display__card"></div>
-        <div className="display__card"></div>
-        <div className="display__card"></div>
-        <div className="display__card"></div>
+        {reviews.map((review, index) => {
+          return (
+            <div className="display__card" key={"review" + index}>
+              <h1>{review.title}</h1>
+              {review.review_img_url && <img
+                src={review.review_img_url}
+                alt={review.title}
+                className="display__image"
+              />}
+              <h2>by {review.designer}</h2>
+              <p>Votes: {review.votes}</p>
+              <p>Comments: {review.comment_count}</p>
+              <p>{review.review_body}</p>
+              <p>Author: {review.owner}</p>
+              <p>Added {dateFormat(review.created_at,"dS mmmm yyyy")}</p>
+            </div>
+          );
+        })}
       </section>
     </main>
   );
