8015fa44792bd57df1ed99c4867c33be17f2ce25
jhunethan <jhunethan23@gmail.com>
2021-07-12 11:56:50 +0100

    nav layout added

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import "./Nav.css"
+
+export default function Nav() {
+  return (
+    <div className="navigationBar">
+      <button className="navigationBar__button">text</button>
+      <button className="navigationBar__button">text</button>
+      <button className="navigationBar__button">text</button>
+      <button className="navigationBar__button">text</button>
+      <button className="navigationBar__button">text</button>
+    </div>
+  );
+}
