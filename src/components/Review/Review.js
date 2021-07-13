69cadcbd0762d538d1d520e2f50fadb84fa8708d
jhunethan <jhunethan23@gmail.com>
2021-07-13 13:11:39 +0100

    updated single review page

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getReviewByReviewId, getCommentsByReviewId } from "../../utils/api";
 import dateFormat from "dateformat";
+import axios from "axios";
 import Header from "../Header/Header";
 
 import "./Review.css";
 
 export default function Review(props) {
   const [review, setReview] = useState();
+  const [votes, setVotes] = useState(0);
   const [comments, setComments] = useState([]);
   const params = useParams();
   const { review_id } = params;
@@ -26,13 +28,35 @@ export default function Review(props) {
     });
   }, [review, review_id]);
 
+  useEffect(() => {
+    if (review) setVotes(review.votes);
+  }, [review]);
+
+  function addVote(event) {
+    event.preventDefault();
+    setVotes((currVotes) => currVotes + 1);
+    axios
+      .patch(`https://ncgames.herokuapp.com/api/reviews/${review.review_id}`, {
+        inc_votes: 1,
+      })
+      .then((response) => {
+        console.log(response);
+      });
+  }
+
   return (
     <section>
       <Header />
       {review && (
         <div className="reviewCard__container">
-          <h1>{review.title}</h1>
-          <h2>by {review.designer}</h2>
+          <h2 className="display__title remove_bold">{review.title}</h2>
+          <h3 className="display__subtitle remove_bold">
+            by {review.designer}
+          </h3>
+          <div className="display__container--spacebetween">
+            <p>{dateFormat(review.created_at, "dS mmmm yyyy")}</p>
+            <p>Written by {review.owner}</p>
+          </div>
           {review.review_img_url && (
             <img
               src={review.review_img_url}
@@ -40,15 +64,14 @@ export default function Review(props) {
               className="display__image"
             />
           )}
-          <p>Votes: {review.votes}</p>
-          <p>Comments: {review.comment_count}</p>
-          <p>{review.review_body}</p>
-          <p>Author: {review.owner}</p>
-          <p>Added {dateFormat(review.created_at, "dS mmmm yyyy")}</p>
+          <p className="display__body">{review.review_body}</p>
+          <p className="btn btn-secondary" onClick={addVote}>
+            ⬆ {votes} Votes
+          </p>
         </div>
       )}
       <section className="comments__section">
-          <h2>Comments</h2>
+        <h2>Comments</h2>
         {comments.map((comment, index) => {
           return (
             <div className="comments__card" key={`comment${index}`}>
